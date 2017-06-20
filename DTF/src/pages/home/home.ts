d8293da52bf83e329b4fa8aa48a22b50456adcf5
MASTEK\Burhan103183 <Burhanuddin.Lakdawala@mastek.com>
2017-06-20 09:09:12 +0530

    Fixing call bug on page 1

--- DTF/src/pages/home/home.ts
+++ DTF/src/pages/home/home.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams , ViewController, ToastController} from 'ionic-angular';
 
 import { NextpagePage } from '..//nextpage/nextpage';
-// import {PeopleServiceProvider} from '../../providers/people-service/people-service';
+
+import { CallNumber } from '@ionic-native/call-number';
 
 @Component({
   selector: 'page-home',
@@ -13,6 +14,7 @@ import { NextpagePage } from '..//nextpage/nextpage';
 export class HomePage {
 
   nextpage = NextpagePage;
+  contact_num: string;
   data: data;
 //  posts : Post[];
 //  posts1 : Postit[];
@@ -33,11 +35,12 @@ export class HomePage {
   // }
   
 
-   constructor(public navCtrl: NavController, public navParams: NavParams, public viewCtrl : ViewController,public toastCtrl: ToastController) {
+   constructor(public navCtrl: NavController, public navParams: NavParams, public viewCtrl : ViewController,public toastCtrl: ToastController, private call: CallNumber) {
   
     // this.postsService.getposts().subscribe(posts => {
     //   this.posts=posts;
 
+    this.contact_num = "+918286204401";
     this.data = {
       location: "abc",
       bus_slot: 123,
@@ -45,6 +48,16 @@ export class HomePage {
       dp: 789,
       date: 0,
       trip_type: 1
+    }
+  }
+
+  async callNumber():Promise<any> {
+    try {
+      await this.call.callNumber(this.contact_num, true);
+    }
+    catch(e)
+    {
+      console.error(e);
     }
   }
 
